5d70163d0e3f61b9585e8867407dd5ee0727bc54
surbhipaneliya182 <135303786+surbhipaneliya182@users.noreply.github.com>
2023-10-10 16:29:30 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardLayout from './layouts/DashboardLayout';
+import Dashboardpage from './layouts/pages/Dashbaord/Dashboardpage';
+
+function App() {
+  return (
+    <>
+    <DashboardLayout>    
+      <Dashboardpage/>
+    </DashboardLayout>
+    </>
+  );
+}
+
+export default App;
